2e9d431a1109be3b91f0a5dc9fe356b3aa854dbd
Joshi Rakesh <jrakesh@affirma.com>
2020-05-11 10:47:30 +0000

    PS automation - Expand/collapse

--- src/aura/EFTStage2/EFTStage2Controller.js
+++ src/aura/EFTStage2/EFTStage2Controller.js
@@ -1,5 +1,21 @@
 ({
 	myAction : function(component, event, helper) {
 		
-	}
+	},
+	
+	expand : function(component, event, helper) {
+    	
+		var icon = component.get("v.AddIconName");
+		var container = component.find("containerCollapsable") ;
+		if(icon == 'utility:add'){
+			$A.util.toggleClass(container, 'hide');
+			component.set('v.AddIconName','utility:dash');
+		}
+		else{
+			$A.util.toggleClass(container, 'hide');
+			component.set('v.AddIconName','utility:add');
+		}
+		
+    }
+	
 })
