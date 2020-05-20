2561727d4e51bbc8f4604121f0a42675389d75f6
Dhwani Bhavsar <dbhavsar@affirma.com>
2020-05-20 21:18:46 +0000

    Routing No related changes

--- src/aura/EFTStage4/EFTStage4Controller.js
+++ src/aura/EFTStage4/EFTStage4Controller.js
@@ -6,14 +6,14 @@
 	LoadBankName : function(component, event, helper) {
 	var action = component.get("c.routingInfo");
         action.setParams({
-            "RoutingNumber": '122105155'
+            "RoutingNumber": component.get("v.RoutingNumber")
         });
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (component.isValid() && state === "SUCCESS") {
                 var result = response.getReturnValue();
 				if(result != undefined && result != '' && result != null)
-					component.set("v.RoutingBankName", result.BankName);
+					component.set("v.RoutingBankName", result);
                 
             }
         });
