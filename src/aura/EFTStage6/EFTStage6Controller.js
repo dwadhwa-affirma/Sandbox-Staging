d232c472079f82807a0191b6dad4faeb4743f534
Dhwani Bhavsar <dbhavsar@affirma.com>
2020-05-23 22:16:15 +0000

    code changes

--- src/aura/EFTStage6/EFTStage6Controller.js
+++ src/aura/EFTStage6/EFTStage6Controller.js
@@ -1,18 +1,21 @@
 ({
 	doInit : function(component, event, helper) {
         debugger;
-		var action = component.get("c.getEFTDetails");
-		var recordId = component.get("v.recordId");	
+		var action = component.get("c.createCase");
+		//var recordId = component.get("v.recordId");	
 		action.setParams({
-		"recordId": recordId
+		"EFTRecord": component.get("v.EFTRecord")
 		});	
 		action.setCallback(this, function(resp) {			
 			var state=resp.getState();			
 			if(state === "SUCCESS"){				
 				var res = resp.getReturnValue();
-				var eft =[];
+                component.set("v.EFTRecord",res);
+				/*var eft =[];
 				eft=res;
-                component.set("v.EFT",eft[0]);
+                component.set("v.EFTRecord",eft[0]);
+                component.set("v.CaseId",eft[0].Case__r.CaseNumber);
+                alert(component.get("v.CaseId"));*/
 				}
 			});
         $A.enqueueAction(action);
