9e6594b9e9931dc874d1c0b314669e2814c95a7a
Dhwani Bhavsar <dbhavsar@affirma.com>
2020-05-23 22:15:59 +0000

    code changes

--- src/aura/EFTStage5/EFTStage5Controller.js
+++ src/aura/EFTStage5/EFTStage5Controller.js
@@ -1,6 +1,7 @@
 ({
 	doInit : function(component, event, helper) {
-		var type = component.get('v.ShareLoanType').split(',')[1];
+        debugger;       
+		var type = component.get('v.EFTRecord.EFT_ID_Type__c');//.split(',')[1];
         if(type != 'Loan'){
             component.set('v.isLoan', false);
         }
@@ -10,18 +11,14 @@
     onMonthChange : function(component, event, helper) {
          debugger;
         var evt = $A.get("e.c:EFTEvent");
-        var PaymentAmt = component.get("v.eftObject2.Payment_Amount__c");
-        var AlternateAmt = component.get("v.eftObject2.Alternate_Amount__c");
-        var EffectiveDate = component.get("v.eftObject2.Effective_Date__c");
-        var Frequency = component.get('v.eftObject2.Frequency__c');
-         var MonthDay = component.get('v.eftObject2.Day_of_Month__c');
+        var PaymentAmt = component.get("v.EFTRecord.Payment_Amount__c");
+        var AlternateAmt = component.get("v.EFTRecord.Alternate_Amount__c");
+        var EffectiveDate = component.get("v.EFTRecord.Effective_Date__c");
+        var Frequency = component.get('v.EFTRecord.Frequency__c');
+         var MonthDay = component.get('v.EFTRecord.Day_of_Month__c');
         
 		if(MonthDay != undefined){
-            evt.setParams({ "PaymentAmt": PaymentAmt , 
-                          "AlternateAmt": AlternateAmt,
-                          "EffectiveDate": EffectiveDate,
-                          "Frequency": Frequency,
-                          "MonthDay": MonthDay});
+            evt.setParams({ "EFTRecord": component.get("v.EFTRecord")});
             evt.fire();
         }
     }
