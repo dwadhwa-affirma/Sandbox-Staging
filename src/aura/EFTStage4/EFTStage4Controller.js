da353e187bd1c4d5ba87c1b811db766ad826325b
Dhwani Bhavsar <dbhavsar@affirma.com>
2020-05-23 22:15:43 +0000

    code changes

--- src/aura/EFTStage4/EFTStage4Controller.js
+++ src/aura/EFTStage4/EFTStage4Controller.js
@@ -7,14 +7,14 @@
 	helper.showSpinner(component);
 	var action = component.get("c.routingInfo");
         action.setParams({
-            "RoutingNumber": component.get("v.RoutingNumber")
+            "RoutingNumber": component.get("v.EFTRecord.Routing_Number__c")
         });
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (component.isValid() && state === "SUCCESS") {
                 var result = response.getReturnValue();
 				if(result != undefined && result != '' && result != null)
-					component.set("v.RoutingBankName", result);
+					component.set("v.EFTRecord.Bank_Name__c", result);
                 
             }
             helper.hideSpinner(component,helper);
@@ -26,16 +26,13 @@
     onTypeChange : function(component, event, helper) {
         debugger;
         var evt = $A.get("e.c:EFTEvent");
-        var RoutingNumber = component.get("v.RoutingNumber");
-        var RoutingBankName = component.get("v.RoutingBankName");
-        var AccountNumber = component.get("v.eftObject.Account_Number__c");
+        var RoutingNumber = component.get("v.EFTRecord.Routing_Number__c");
+        var RoutingBankName = component.get("v.EFTRecord.Bank_Name__c");
+        var AccountNumber = component.get("v.EFTRecord.Account_Number__c");
         var Type = event.getSource().get('v.value');
         
 		if(Type != undefined){
-            evt.setParams({ "RoutingNumber": RoutingNumber , 
-                          "RoutingBankName": RoutingBankName,
-                          "AccountNumber": AccountNumber,
-                          "Type": Type});
+            evt.setParams({ "EFTRecord": component.get("v.EFTRecord")});
             evt.fire();
         }
         
