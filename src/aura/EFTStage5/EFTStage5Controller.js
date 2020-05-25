71aaff26431cea51242456d5fce5620b567c697e
Dhwani Bhavsar <dbhavsar@affirma.com>
2020-05-25 18:44:39 +0000

    EFT Changes

--- src/aura/EFTStage5/EFTStage5Controller.js
+++ src/aura/EFTStage5/EFTStage5Controller.js
@@ -4,7 +4,9 @@
 		var type = component.get('v.EFTRecord.EFT_ID_Type__c');//.split(',')[1];
         if(type != 'Loan'){
             component.set('v.isLoan', false);
+            component.set('v.EFTRecord.Frequency__c', 'Monthly');
         }
+        
        
 	},
     
@@ -16,6 +18,7 @@
         var EffectiveDate = component.get("v.EFTRecord.Effective_Date__c");
         var Frequency = component.get('v.EFTRecord.Frequency__c');
          var MonthDay = component.get('v.EFTRecord.Day_of_Month__c');
+        component.set("v.EFTRecord.Stage__c",'Payment Monthly');
         
 		if(MonthDay != undefined){
             evt.setParams({ "EFTRecord": component.get("v.EFTRecord")});
