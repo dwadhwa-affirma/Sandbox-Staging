30f70f9d927404a714fc3a8bf4c5cf6207c4f72d
Dhwani Bhavsar <dbhavsar@affirma.com>
2020-07-07 20:12:27 +0000

    Bug Fixes

--- src/aura/EFTStage6/EFTStage6Controller.js
+++ src/aura/EFTStage6/EFTStage6Controller.js
@@ -6,7 +6,7 @@
             component.set("v.UpdateText","Expire");
         }
         
-        if(component.get("v.EFTRecord.Action_Type__c") != 'Expire' && !component.get("v.EFTRecord.Expired__c")
+        if(component.get("v.isDocusignEmailSelected") && component.get("v.EFTRecord.Action_Type__c") != 'Expire' && !component.get("v.EFTRecord.Expired__c")
            && component.get("v.EFTRecord.Action_Type__c") != 'View'){
 		var action = component.get("c.createCase");
 		//var recordId = component.get("v.recordId");	
@@ -22,12 +22,29 @@
 				{
                     helper.sendACHDocument(component, event, helper);
                 }
-				/*var eft =[];
-				eft=res;
-                component.set("v.EFTRecord",eft[0]);
-                component.set("v.CaseId",eft[0].Case__r.CaseNumber);
-                alert(component.get("v.CaseId"));*/
+				
 				}
+			});
+        $A.enqueueAction(action);
+        }
+        
+        else if(component.get("v.isDocusignEmailSelected") == false 
+        		&& component.get("v.EFTRecord.Action_Type__c") != 'Expire' 
+        		&& !component.get("v.EFTRecord.Expired__c")
+        		&& component.get("v.EFTRecord.Action_Type__c") != 'View'){
+		var action = component.get("c.getEpisysEmailAddresses");
+		var recordId = component.get("v.recordId");	
+		action.setParams({
+		"recordId": component.get("v.recordId")
+		});	
+		action.setCallback(this, function(resp) {			
+			var state=resp.getState();			
+			if(state === "SUCCESS"){				
+				var res = resp.getReturnValue();
+                var emailList = res["EmailIds"];
+                component.set("v.EmailIdsList",emailList);
+                	
+			}
 			});
         $A.enqueueAction(action);
         }
@@ -56,6 +73,20 @@
         $A.enqueueAction(action);
         }
 	},
+	
+	onEmailChange: function (component, event, helper) {
+    	var MemberEmail = event.getSource().get('v.value');    
+    	component.set("v.EFTRecord.Docusign_Member_Email__c",MemberEmail);
+    	
+    	var evt = $A.get("e.c:EFTEvent");
+        var EFT = component.get("v.EFTRecord");
+        var isDocusignEmailSelected = true;
+        
+		if(EFT != undefined){
+            evt.setParams({ "EFTRecord": EFT, "isDocusignEmailSelected": isDocusignEmailSelected});
+            evt.fire();
+        }
+    }
 	
 	
 })
