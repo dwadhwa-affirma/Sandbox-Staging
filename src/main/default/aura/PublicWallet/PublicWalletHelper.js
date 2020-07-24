15717da671f07dcf64e798fd8c9c4c02710ea405
Deepika Wadhwa <dwadhwa@affirma.com>
2020-07-24 11:58:22 +0530

    fix for public wallet button color during relaunch maap

--- src/main/default/aura/PublicWallet/PublicWalletHelper.js
+++ src/main/default/aura/PublicWallet/PublicWalletHelper.js
@@ -58,82 +58,85 @@
 				if(result.DefaultEmailInList != undefined){
 					component.set("v.DefaultEmailSelected", result.DefaultEmailInList);
 				}
-				if(result.PublicWalletStatus != undefined){
 				
-					if(component.get("v.IsUserSessionLoaded") == true)
-					{
-							if(result.DOBMatch != undefined)
-							{
-								if(result.DOBMatch.length > 0){
-									if(result.DOBMatch == 'Pass'){
-											component.find('DobPassButton').set("v.variant", "success");
-											component.find('DobFailButton').set("v.variant", "neutral");
-									}
-									else{
-											component.find('DobFailButton').set("v.variant", "destructive");
-											component.find('DobPassButton').set("v.variant", "neutral");
-									}
-								}
-								
-							}
-							if(result.IdNumberMatch != undefined)
-							{	
-								if(result.IdNumberMatch.length > 0){
-									if(result.IdNumberMatch == 'Pass'){
-											component.find('DLPassButton').set("v.variant", "success");
-											component.find('DLFailButton').set("v.variant", "neutral");
-									}
-									else{
-											component.find('DLFailButton').set("v.variant", "destructive");
-											component.find('DLPassButton').set("v.variant", "neutral");
-									}
-								}
-								
-							}
-							if(result.MMNMatch != undefined)
-							{				
-									if(result.MMNMatch.length > 0){
-										if(result.MMNMatch == 'Pass'){
-												component.find('MothersMaidenNamePassButton').set("v.variant", "success");
-												component.find('MothersMaidenNameFailButton').set("v.variant", "neutral");
+				
+				if(component.get("v.IsUserSessionLoaded") == true)
+				{
+					if(result.ReLaunchAuthPublicWalletStatus != undefined)
+						{
+							if(result.ReLaunchAuthDOBMatch != undefined)
+								{
+									if(result.ReLaunchAuthDOBMatch.length > 0){
+										if(result.ReLaunchAuthDOBMatch == 'Pass'){
+												component.find('DobPassButton').set("v.variant", "success");
+												component.find('DobFailButton').set("v.variant", "neutral");
 										}
 										else{
-												component.find('MothersMaidenNameFailButton').set("v.variant", "destructive");
-												component.find('MothersMaidenNamePassButton').set("v.variant", "neutral");
+												component.find('DobFailButton').set("v.variant", "destructive");
+												component.find('DobPassButton').set("v.variant", "neutral");
 										}
 									}
 									
-							}
-							if(result.EmailMatch != undefined)
-							{
-								if(result.EmailMatch.length > 0){
-									if(result.EmailMatch == 'Pass'){
-											component.find('EmailPassButton').set("v.variant", "success");
-											component.find('EmailFailButton').set("v.variant", "neutral");
-									}
-									else{
-											component.find('EmailFailButton').set("v.variant", "destructive");
-											component.find('EmailPassButton').set("v.variant", "neutral");
-									}
 								}
-								
-							}
-							if(result.AdditionalTokenMatch != undefined)
-							{
-								if(result.AdditionalTokenMatch.length > 0){
-									if(result.AdditionalTokenMatch == 'Pass'){
-											component.find('TokenPassButton1').set("v.variant", "success");
-											component.find('TokenFailButton1').set("v.variant", "neutral");
+								if(result.ReLaunchAuthIdNumberMatch != undefined)
+								{	
+									if(result.ReLaunchAuthIdNumberMatch.length > 0){
+										if(result.ReLaunchAuthIdNumberMatch == 'Pass'){
+												component.find('DLPassButton').set("v.variant", "success");
+												component.find('DLFailButton').set("v.variant", "neutral");
+										}
+										else{
+												component.find('DLFailButton').set("v.variant", "destructive");
+												component.find('DLPassButton').set("v.variant", "neutral");
+										}
 									}
-									else{
-											component.find('TokenFailButton1').set("v.variant", "destructive");
-											component.find('TokenPassButton1').set("v.variant", "neutral");
-									}
+									
 								}
+								if(result.ReLaunchAuthMMNMatch != undefined)
+								{				
+										if(result.ReLaunchAuthMMNMatch.length > 0){
+											if(result.ReLaunchAuthMMNMatch == 'Pass'){
+													component.find('MothersMaidenNamePassButton').set("v.variant", "success");
+													component.find('MothersMaidenNameFailButton').set("v.variant", "neutral");
+											}
+											else{
+													component.find('MothersMaidenNameFailButton').set("v.variant", "destructive");
+													component.find('MothersMaidenNamePassButton').set("v.variant", "neutral");
+											}
+										}
+										
+								}
+								if(result.ReLaunchAuthEmailMatch != undefined)
+								{
+									if(result.ReLaunchAuthEmailMatch.length > 0){
+										if(result.ReLaunchAuthEmailMatch == 'Pass'){
+												component.find('EmailPassButton').set("v.variant", "success");
+												component.find('EmailFailButton').set("v.variant", "neutral");
+										}
+										else{
+												component.find('EmailFailButton').set("v.variant", "destructive");
+												component.find('EmailPassButton').set("v.variant", "neutral");
+										}
+									}
+									
+								}
+								if(result.ReLaunchAuthAdditionalTokenMatch != undefined)
+								{
+									if(result.ReLaunchAuthAdditionalTokenMatch.length > 0){
+										if(result.ReLaunchAuthAdditionalTokenMatch == 'Pass'){
+												component.find('TokenPassButton1').set("v.variant", "success");
+												component.find('TokenFailButton1').set("v.variant", "neutral");
+										}
+										else{
+												component.find('TokenFailButton1').set("v.variant", "destructive");
+												component.find('TokenPassButton1').set("v.variant", "neutral");
+										}
+									}
+									
+								}
+						}
 								
-							}
-						
-					}
+					
 				
 				
 					if(result.PublicWalletStatus =='Fail' && component.get("v.IsUserSessionLoaded") == false)
