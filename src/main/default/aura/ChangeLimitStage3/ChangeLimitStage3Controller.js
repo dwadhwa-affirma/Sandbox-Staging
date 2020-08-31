b34810136d777ceed26195f99a5189bbcd2f4f02
Rakesh Joshi <jrakesh@affirma.com>
2020-08-31 17:58:02 +0530

    CRM-1814

--- src/main/default/aura/ChangeLimitStage3/ChangeLimitStage3Controller.js
+++ src/main/default/aura/ChangeLimitStage3/ChangeLimitStage3Controller.js
@@ -21,6 +21,7 @@
     	
     	var evt = $A.get("e.c:ChangeLimitEvent");
         var CL = component.get("v.CLRecord");
+        
         var isMemberSelected = component.get("v.isMemberSelected");
         
         if(CL != undefined){
@@ -35,6 +36,11 @@
         var evt = $A.get("e.c:ChangeLimitEvent");
         var CL = component.get("v.CLRecord");
         var isMemberSelected = component.get("v.isMemberSelected");
+        var ATM = component.get("v.CLRecord.ATM_Usage_Limit__c");
+        var AUTH = component.get("v.CLRecord.Auth_POS_Limit__c");
+        
+        component.set("v.CLRecord.ATM_Usage_Limit__c",ATM);
+        component.set("v.CLRecord.Auth_POS_Limit__c",AUTH);
         
         if(CL != undefined){
             evt.setParams({ "CLRecord": CL, "isMemberSelected": isMemberSelected});
