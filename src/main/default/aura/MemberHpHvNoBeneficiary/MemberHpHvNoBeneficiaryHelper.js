60d6abdd9258da96193a09ea395ef55b31fbc81b
Dhwani Bhavsar <dbhavsar@affirma.com>
2020-10-08 14:42:35 +0530

    CRM-1864, CRM-1865

--- src/main/default/aura/MemberHpHvNoBeneficiary/MemberHpHvNoBeneficiaryHelper.js
+++ src/main/default/aura/MemberHpHvNoBeneficiary/MemberHpHvNoBeneficiaryHelper.js
@@ -1,34 +1,39 @@
 ({
-    GetData : function(component, event, helper, RecordId) {
-        debugger;
-		var action = component.get("c.getData");
-            //action.setParams({"LogId": LogId});
-            component.set('v.columns', [
-                {label: 'Account Name', fieldName: 'URL', type: 'url',typeAttributes: { 
-                    label: {
-                        fieldName: 'Name'
-                    },
-                    target: '_self' 
-                }},
-                    {label: 'Member Branch', fieldName: 'Member_Branch__c', type: 'text'},
-                    {label: 'Phone', fieldName: 'Phone', type: 'Phone'},
-                    {label: 'High Value Flag', fieldName: 'High_Value_Flag__c', type: 'text'}
-                ]);
-		    action.setCallback(this, function (response) {
-			var status = response.getState();            
-				if (component.isValid() && status === "SUCCESS") {                        
-                        var result = response.getReturnValue();
-                        if(result.length > 0){
-                            // set listViewResult attribute with response
-                            
-                            result.forEach(function(item) {
-                                item['URL'] = '/lightning/r/Account/' + item['Id'] + '/view';
-                            });
-                            component.set("v.listViewResult",result);
-                               
-                        }   
-                }
+    GetData: function (component, event, helper, pageNumber, pageSize) {
+      debugger;
+      var action = component.get("c.getData");
+      var MemberBranch = component.get("v.MemberBranch");
+      var HighValueFlag = component.get("v.HighValueFlag");
+      component.set("v.loading", true);
+      action.setParams({
+        MemberBranchQuery: MemberBranch,
+        HighValueFlagQuery: HighValueFlag,
+        pageNumber: pageNumber,
+        pageSize: pageSize,
+      });
+  
+      action.setCallback(this, function (response) {
+        var status = response.getState();
+        if (component.isValid() && status === "SUCCESS") {
+          var resultData = response.getReturnValue();
+          component.set("v.listViewResult", resultData.accountList);
+          component.set("v.PageNumber", resultData.pageNumber);
+          component.set("v.TotalRecords", resultData.totalRecords);
+          component.set("v.RecordStart", resultData.recordStart);
+          component.set("v.RecordEnd", resultData.recordEnd);
+          component.set(
+            "v.TotalPages",
+            Math.ceil(resultData.totalRecords / pageSize)
+          );
+          if (resultData.accountList.length > 0) {          
+              resultData.accountList.forEach(function (item) {
+              item["URL"] = "/lightning/r/Account/" + item["Id"] + "/view";
             });
-            $A.enqueueAction(action); 
-    }
-})
+          }        
+        }
+        component.set("v.loading", false);
+      });
+      $A.enqueueAction(action);
+      
+    },
+  });
