795534d96533627f2969c66bb569dd6897b17f21
Rakesh Joshi <jrakesh@affirma.com>
2020-11-20 15:19:46 +0530

    OR-140

--- src/main/default/lwc/leadWithCompanyLwc/leadWithCompanyLwc.js
+++ src/main/default/lwc/leadWithCompanyLwc/leadWithCompanyLwc.js
@@ -1,9 +1,12 @@
 import { LightningElement,api,track, wire } from 'lwc';
 import fetchLeads from '@salesforce/apex/LeadWithCompanyController.fetchLeads';
+import delSelectedLeads from '@salesforce/apex/LeadWithCompanyController.deleteLeads';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import {refreshApex} from '@salesforce/apex';
 import { NavigationMixin } from 'lightning/navigation'; ///Navigation
 
 const actions = [
-    { label: 'View', name: 'show_details' },
+    { label: 'Delete', name: 'delete'}
 ];
 
 const columns = [
@@ -19,6 +22,10 @@ const columns = [
     { label: 'Company', fieldName: 'Company', type: 'text', sortable:'true'  },
     { label: 'Email', fieldName: 'Email', type: 'email' , sortable:'true' },
     { label: 'Phone', fieldName: 'Phone', type: 'phone', sortable:'true'  },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    }, 
     
 ];
 
@@ -28,6 +35,15 @@ export default class WrapperClassTestNavigation extends LightningElement {
     @track listAccsData = [];
     @track error;
     @track columns = columns;
+    refreshTable;
+    @wire(fetchLeads,{lead:'$recordId'})
+    relations(result) {
+        this.refreshTable = result;
+        if (result.data) {
+            this.data = result.data;
+            this.emptyList = true;
+        }
+    }
 
     @wire(fetchLeads,{lead:'$recordId'})
         test({ error, data }) {
@@ -40,16 +56,68 @@ export default class WrapperClassTestNavigation extends LightningElement {
         }
     
 
+
     navigateToRecordViewPage(event) {
-        this.record = event.detail.row;
+        let row = event.detail.row;
+        let actionName = event.detail.action.name;
         // View a custom object record.
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: this.record.id,
-                objectApiName: 'Lead', // objectApiName is optional
-                actionName: 'view'
-            }
+        switch (actionName) {
+            case 'record_details':
+                this.viewCurrentRecord(row);
+                break;
+            case 'edit':
+                this.editCurrentRecord(row);
+                break;
+            case 'delete':
+                this.deleteCons(row);
+                break;
+        }
+    }
+
+    viewCurrentRecord(currentRow) {
+        this.bShowModal = true;
+        this.isEditForm = false;
+        this.record = currentRow;
+    }
+
+    editCurrentRecord(currentRow) {
+        // open modal box
+        this.bShowModal = true;
+        this.isEditForm = true;
+
+        // assign record id to the record edit form
+        this.currentRecordId = currentRow.Id;
+    }
+
+    deleteCons(currentRow) {
+        let currentRecord = [];
+        currentRecord.push(currentRow.Id);
+        this.showLoadingSpinner = true;
+
+        // calling apex class method to delete the selected contact
+        delSelectedLeads({listLeads: currentRecord})
+        .then(result => {
+            window.console.log('result ====> ' + result);
+            this.showLoadingSpinner = false;
+
+            // showing success message
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Success!!',
+                message: currentRow.FirstName + ' '+ currentRow.LastName +' Lead deleted.',
+                variant: 'success'
+            }),);
+
+            // refreshing table data using refresh apex
+             return refreshApex(this.refreshTable);
+
+        })
+        .catch(error => {
+            window.console.log('Error ====> '+error);
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error!!', 
+                message: error.message, 
+                variant: 'error'
+            }),);
         });
     }
 
