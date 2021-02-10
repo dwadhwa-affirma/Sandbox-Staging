338ad781d4a8152fcf979dbfb3da8822d09f0c29
Rakesh Joshi <jrakesh@affirma.com>
2021-02-10 15:30:26 +0530

    CRM-1814

--- src/main/default/aura/CardLimitResetStage3/CardLimitResetStage3Controller.js
+++ src/main/default/aura/CardLimitResetStage3/CardLimitResetStage3Controller.js
@@ -1,24 +1,53 @@
 ({
 	doInit : function(component, event, helper) {
 	    
+        var action = component.get("c.getLimits");
 		var recordId = component.get("v.recordId");
 		var CLRecord = component.get("v.CLRecord");
+        var membername = CLRecord.Member_Name__c;
+        var cardnumber = CLRecord.Card_Number__c;
+        var type = CLRecord.Type__c;
         var isMemberSelected = component.get("v.isMemberSelected");
         var evt = $A.get("e.c:CardLimitResetEvent");
-        
-        var type = CLRecord.Type__c;
+        var ATM = component.get("v.CLRecord.ATM_Usage_Limit__c");
+        var AUTH = component.get("v.CLRecord.Auth_POS_Limit__c");
+      
         if(type == 'Change Card Limits'){
-        	component.set("v.ChangeCardLimitIsEnabled",true);
-            component.set("v.CLRecord.ATM_Usage_Limit__c",'510.00');
-            component.set("v.CLRecord.Auth_POS_Limit__c",'2500.00');
+            
+            component.set("v.ChangeCardLimitIsEnabled",true);
+            action.setParams({
+                "recordId": recordId,
+                "cardnumber": cardnumber,
+                "membername": membername
+            });
+            
+            action.setCallback(this, function(resp) {
+                var state=resp.getState();			
+                if(state === "SUCCESS"){
+                    var result =  resp.getReturnValue();
+                    if(result != undefined){
+                        if(result.PosLimit != undefined && result.PosLimit != ''){
+                            component.set("v.CLRecord.Auth_POS_Limit__c", result.PosLimit);
+                        }
+                        else{
+                            component.set("v.CLRecord.Auth_POS_Limit__c",'2500.00');
+                        }
+                        if(result.AuthLimit != undefined && result.AuthLimit != ''){
+                            component.set("v.CLRecord.ATM_Usage_Limit__c", result.AuthLimit);
+                        }
+                        else{
+                        	component.set("v.CLRecord.ATM_Usage_Limit__c",'510.00');   
+                        }
+                        evt.setParams({ "CLRecord": CLRecord, "isMemberSelected": isMemberSelected});
+            			evt.fire();
+                    }
+                }
+            });
+            $A.enqueueAction(action);
         }
-        else
+        else{
 			component.set("v.ChangeCardLimitIsEnabled",false);
-        
-        if(CLRecord != undefined && type == 'Change Card Limits'){
-            evt.setParams({ "CLRecord": CLRecord, "isMemberSelected": isMemberSelected});
-            evt.fire();
-    	}
+        }
 
 	},
     
