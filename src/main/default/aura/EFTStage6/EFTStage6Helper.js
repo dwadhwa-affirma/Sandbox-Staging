a60e65c1e817cea30dfc230412d30136da76ddfe
Dhwani Bhavsar <dbhavsar@affirma.com>
2021-02-19 12:43:00 +0530

    SI11042-186

--- src/main/default/aura/EFTStage6/EFTStage6Helper.js
+++ src/main/default/aura/EFTStage6/EFTStage6Helper.js
@@ -40,11 +40,21 @@
 			var resultData = result.getReturnValue();
 			if (resultData.isValid == "false") {
 			  component.set("v.EFTRecord.Effective_Date__c", "");
-			  alert(resultData.ErrorMessage.replace("Effective","Expire"));
+			  component.set('v.IsCountACHpaymentPostVisible', false);
+			  alert(resultData.ErrorMessage.replace("Effective","Expire"));			  
 			}
 			else{
 				var evt = $A.get("e.c:EFTEvent");
 				var EFT = component.get("v.EFTRecord");
+				var endDate = new Date(component.get("v.EFTRecord.Effective_Date__c"));
+				var startDate = new Date(component.get("v.EFTRecord.EFT_Payment_Date__c"));
+
+				var Diff = Math.floor(Math.round((endDate-startDate)/(1000*60*60*24))/30);
+				var Occurances = Diff +1 ;
+				component.set('v.CountACHpaymentPost', Occurances);
+				component.set('v.IsCountACHpaymentPostVisible', true);
+				//(endDate.getFullYear()*12+endDate.getMonth())-(startDate.getFullYear()*12+startDate.getMonth());
+
 				
 				if(EFT != undefined){
 					evt.setParams({ "EFTRecord": EFT});
