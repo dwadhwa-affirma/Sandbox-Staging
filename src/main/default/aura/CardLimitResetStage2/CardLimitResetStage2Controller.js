2d1e298ec10e51b8963bce4a23828386e3ae36bf
Rakesh Joshi <jrakesh@affirma.com>
2021-07-05 15:41:44 +0530

    CRM-1814

--- src/main/default/aura/CardLimitResetStage2/CardLimitResetStage2Controller.js
+++ src/main/default/aura/CardLimitResetStage2/CardLimitResetStage2Controller.js
@@ -67,28 +67,55 @@
     	var SelectedCardNumber = event.getSource().get('v.value');
         var SelectedCardLocator;
         var SelectedAccountNumber;
-        
-        var map=component.get('v.CardListMap');
-        for(var i=0;i<map.length;i++){
-        	if(map[i].CardNumber == SelectedCardNumber){
-            	SelectedCardLocator = map[i].CardLocator;
-                SelectedAccountNumber = map[i].AccountNumber;
-                break;
-            }
-        }
-    	component.set("v.CLRecord.Card_Number__c",SelectedCardNumber);
-        component.set("v.CLRecord.Card_Locator__c",SelectedCardLocator);
-        component.set("v.CLRecord.Member_Number__c",SelectedAccountNumber);
-    	
-    	var evt = $A.get("e.c:CardLimitResetEvent");
         var CL = component.get("v.CLRecord");
+        var evt = $A.get("e.c:CardLimitResetEvent");
         var isMemberSelected = component.get("v.isMemberSelected");
         
+        var action = component.get("c.getCardDetails");
+        var recordId = component.get("v.recordId");
+		var CLRecord = component.get("v.CLRecord");
+        var memberName = CLRecord.Member_Name__c;
         
-        if(CL != undefined){
-            evt.setParams({ "CLRecord": CL, "isMemberSelected": isMemberSelected});
-            evt.fire();
-    	}
+        action.setParams({
+			"recordId": recordId,
+            "sObjectType": memberName,
+            "SelectedCardNumber":SelectedCardNumber
+		});
+        
+        action.setCallback(this, function(resp) {
+			var state=resp.getState();			
+			if(state === "SUCCESS"){
+				var result =  resp.getReturnValue();
+                
+                if(result != undefined){
+                    if(result.CardList != undefined && result.CardList != ''){
+                   		component.set('v.CardListMap', result.CardList);
+                    }
+               	    var map=component.get('v.CardListMap');
+                
+                    for(var i=0;i<map.length;i++){
+                        if(map[i].CardNumber == SelectedCardNumber){
+                            SelectedCardLocator = map[i].CardLocator;
+                            SelectedAccountNumber = map[i].AccountNumber;
+                            break;
+                        }
+                    }
+                    component.set("v.CLRecord.Card_Number__c",SelectedCardNumber);
+                    component.set("v.CLRecord.Card_Locator__c",SelectedCardLocator);
+                    component.set("v.CLRecord.Member_Number__c",SelectedAccountNumber);
+                    
+                    if(SelectedAccountNumber != undefined){
+                        evt.setParams({ "CLRecord": CL, "isMemberSelected": isMemberSelected});
+                        evt.fire();
+                    }
+                    
+                    
+                }
+			}
+		});
+        
+        $A.enqueueAction(action);
+       
     }
   
 })
